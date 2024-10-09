10b68c7d1b769a52524faf2c8c56d441e60e75ea
iamkishan <100196912+KishanKakadia96@users.noreply.github.com>
2024-10-09 23:49:38 +0200

    Implement authentication components and improve form styling
    Add Login and Register components, implement NotFound page, and enhance form styling in Form.css

--- Conversebot-frontend/src/pages/Login.jsx
+++ Conversebot-frontend/src/pages/Login.jsx
@@ -1,5 +1,7 @@
+import Form from "../components/Form"
+
 function Login() {
-    return <div>Login page</div>
+    return <Form route="/token/" method="login" />
 }
 
-export default Login;
+export default Login
