fbf53b154a3bc1907894670535224b9a27f46af4
iamkishan <100196912+KishanKakadia96@users.noreply.github.com>
2024-10-09 23:51:16 +0200

    Enhance routing and implement user authentication

--- Conversebot-frontend/src/App.jsx
+++ Conversebot-frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import react from "react"
+import React from "react"
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
@@ -6,21 +6,23 @@ import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
 import ProtectedRoute from "./components/ProtectedRoute"
 
+// Logout component to clear localStorage and redirect to login page
 function Logout() {
   localStorage.clear()
   return <Navigate to="/login" />
 }
 
+// RegisterAndLogout component clears localStorage and renders the Register page
 function RegisterAndLogout() {
   localStorage.clear()
   return <Register />
 }
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
+        {/* Protected route for the home page, accessible only after login */}
         <Route
           path="/"
           element={
@@ -29,10 +31,12 @@ function App() {
             </ProtectedRoute>
           }
         />
+        {/* Public routes for login, register, and logout */}
         <Route path="/login" element={<Login />} />
         <Route path="/logout" element={<Logout />} />
         <Route path="/register" element={<RegisterAndLogout />} />
-        <Route path="*" element={<NotFound />}></Route>
+        {/* Fallback route for 404 - Not Found */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   )
